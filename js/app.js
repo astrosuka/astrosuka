c3e50c33696c50e9451e99c4e7282992685e4388
astrosuka <yegresvoselok@gmail.com>
2024-01-27 15:56:39 -0300

    Create object template for sound works list

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const soundWorks = [
+    {
+        artist: '',
+        release: '',
+        label: '',
+        year: '',
+        role: '',
+        link: ''
+    },
+    {
+        atist: 'Yoto',
+        release: 'Levure',
+        label: 'Kit Records',
+        year: '2024',
+        role: 'Mixing',
+        link: 'https://yoto.bandcamp.com/album/levure'
+    },
+    {
+        artist: '',
+        release: '',
+        label: '',
+        year: '',
+        role: '',
+        link: ''
+    },
+    {
+        artist: '',
+        release: '',
+        label: '',
+        year: '',
+        role: '',
+        link: ''
+    },
+    
+];

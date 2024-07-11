c34d370b7350997d7e139a1b70944d65aa49c33d
astrosuka <yegresvoselok@gmail.com>
2024-07-11 02:13:52 -0300

    Remove sanity

--- astrosuka/schemaTypes/index.js
+++
@@ -1,8 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import link from './link'
-import about from './about'
-
-export const schemaTypes = [post, author, category, blockContent, link, about]

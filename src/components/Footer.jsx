cb95cb97754e3273b1d7b261fc708efe4244a3d5
astrosuka <yegresvoselok@gmail.com>
2024-08-24 01:26:54 -0300

    Adjust padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const { language, setLanguage } = useContext(LanguageContext);
 
   return (
-    <footer className="fixed bottom-3 left-6 flex flex-row-reverse gap-2 text-xs md:left-auto md:right-6 md:flex-row md:text-sm">
+    <footer className="fixed bottom-6 left-6 flex flex-row-reverse gap-2 text-xs md:left-auto md:right-6 md:flex-row md:text-sm">
       <button
         className="rounded-sm border border-slate-300 bg-slate-100 px-2 text-black sm:hover:font-bold sm:hover:invert"
         onClick={() => setLanguage(language === "en" ? "es" : "en")}
